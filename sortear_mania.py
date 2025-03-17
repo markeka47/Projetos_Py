22d834f5e76d16a76340708a0ed3129113f79406
Marcos Santos <markeka.mar@gmail.com>
2025-03-17 10:33:44 -0400

    atualizar

--- sortear_mania.py
+++ sortear_mania.py
@@ -1,26 +1,26 @@
 import random
 
 # Define a list of numbers
-col1 = [1, 11, 21, 31, 41, 51, 61, 71, 81, 91]
-col2 = [2, 12, 22, 32, 42, 52, 62, 72, 82, 92]
-col3 = [3, 13, 23, 33, 43, 53, 63, 73, 83, 93]
-col4 = [4, 14, 24, 34, 44, 54, 64, 74, 84, 94]
-col5 = [5, 15, 25, 35, 45, 55, 65, 75, 85, 95]
-col6 = [6, 16, 26, 36, 46, 56, 66, 76, 86, 96]
-col7 = [7, 17, 27, 37, 47, 57, 67, 77, 87, 97]
-col8 = [8, 18, 28, 38, 48, 58, 68, 78, 88, 98]
-col9 = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
-col10 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+col1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+col2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+col3 = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+col4 = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+col5 = [41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+col6 = [51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
+col7 = [61, 62, 63, 64, 65, 66, 67, 68, 69, 70]
+col8 = [71, 72, 73, 74, 75, 76, 77, 78, 79, 80]
+col9 = [81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
+col10 = [91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
 
 # Randomly select 2 unique numbers from the list
 selected_col1 = random.sample(col1, 3)
-selected_col2 = random.sample(col2, 3)
-selected_col3 = random.sample(col3, 3)
-selected_col4 = random.sample(col4, 4)
-selected_col5 = random.sample(col5, 3)
-selected_col6 = random.sample(col6, 3)
-selected_col7 = random.sample(col7, 3)
-selected_col8 = random.sample(col8, 3)
+selected_col2 = random.sample(col2, 5)
+selected_col3 = random.sample(col3, 5)
+selected_col4 = random.sample(col4, 3)
+selected_col5 = random.sample(col5, 6)
+selected_col6 = random.sample(col6, 6)
+selected_col7 = random.sample(col7, 6)
+selected_col8 = random.sample(col8, 6)
 selected_col9 = random.sample(col9, 5)
 selected_col10 = random.sample(col10, 5)
 
